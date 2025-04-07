1d28aaa069aa7a6d5b4f8cf0b834e70772a8a815
Augusto <amilani@itba.edu.ar>
2025-04-07 13:38:19 -0300

    pequeño ajuste

--- Lequel.cpp
+++ Lequel.cpp
@@ -44,7 +44,7 @@
             if(wstrTrigram.length() == 3) {
                 // Tip: convert wstring to UTF-8 string
                 std::string trigram = converter.to_bytes(wstrTrigram);
-                profile[trigram] = 999.0F;    //Frequency of trigram is incremented by one.
+                profile[trigram] += 1.0F;    //Frequency of trigram is incremented by one.
             }
 
             unicodeString = unicodeString.substr(1);   // substracts first character from line.
